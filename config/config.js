47a6649804486ce6b15563d7969f5beaabc6f3cf
Angelo Carly <angelo.carly.y7553@student.hogent.be>
2018-11-09 09:14:13 +0100

    switch to test db

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
       password: 'Team4Wachtwoord',
       server: '141.134.155.219', // You can use 'localhost\\instance' to connect to named instance
       port: 1433,
-      database: 'Mindfulness', //production
+      database: 'test', //production
       pool: {
         min: 1 //There must be at least 1 concurrent
       },
