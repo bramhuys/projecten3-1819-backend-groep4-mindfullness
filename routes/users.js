1bda3152da61812dc36e15fa7c0e118de1c251e5
Stijn De Raeve-Zenner <stijn.deraevezenner@student.hogent.be>
2018-12-07 14:10:08 +0100

    another delete test

--- routes/users.js
+++ routes/users.js
@@ -170,12 +170,16 @@
 
     router.delete('/:uid', (req, res) => {
       var uid = req.params.uid;
-      admin.auth().deleteUser(uid)
-        .then(function () {
-          console.log("Successfully deleted user");
-        })
-        .catch(function (error) {
-          console.log("Error deleting user:", error);
+
+      const admin = require('firebase-admin');
+      admin.initializeApp();
+
+      exports.deleteUser = functions.firestore
+        .document('users/' + uid)
+        .onDelete((snap, context) => {
+          return admin.auth().deleteUser(snap.id)
+            .then(() => console.log('Deleted user with ID:' + snap.id))
+            .catch((error) => console.error('There was an error while deleting user:', error));
         });
     });
 
